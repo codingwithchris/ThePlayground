7359d11f0e5bf7caa3bc3ccd03cd1e6b2bb455cd
Chris Hahn <chris+digital@chrishahn.me>
2021-09-15 21:22:03 -0400

    Fix double slash issue

--- packages/web/@gatsby/node-api/index.ts
+++ packages/web/@gatsby/node-api/index.ts
@@ -10,8 +10,6 @@ import {
     BuildArgs as GatsbyNodeBuildArgs,
 } from 'gatsby';
 
-import { normalizeSlug, buildNestedSlugPath } from '@web/shared/utils';
-
 import path from 'path';
 
 // ! I have no idea why, but importing from `@web/domains/performance/season` breaks with `SyntaxError: Unexpected token '<'`
@@ -213,7 +211,7 @@ const generateSeasonsAndShows = async ({
     data.allSanitySeason.nodes.forEach((season) => {
         const seasonConfig: SeasonPageConfig = {
             slug: season.slug.current,
-            url: buildNestedSlugPath([SEASON_ROOT_SLUG, season.slug.current])!,
+            url: `/${SEASON_ROOT_SLUG}/${season.slug.current}/`,
             id: season._id,
             template: path.resolve(
                 `./src/domains/performance/season/__template__/SeasonTemplate.tsx`
@@ -244,10 +242,7 @@ const generateSeasonsAndShows = async ({
 
             const showConfig: ShowPageConfig = {
                 slug: show.slug.current,
-                url: buildNestedSlugPath([
-                    seasonConfig.url,
-                    show.slug.current,
-                ])!,
+                url: `${seasonConfig.url}${show.slug.current}/`,
                 id: show._id,
                 template: path.resolve(
                     `./src/domains/performance/show/__template__/ShowTemplate.tsx`
@@ -312,7 +307,7 @@ const generateBlogPosts = async ({
     data.allSanityPost.nodes.forEach((post) => {
         const blogConfig: BlogPostConfig = {
             slug: post.slug.current,
-            url: buildNestedSlugPath([blogParentPage!, post.slug.current])!,
+            url: `/${blogParentPage}/${post.slug.current}/`,
             id: post._id,
             template: path.resolve(
                 `./src/domains/blog/__template__/PostTemplate.tsx`
