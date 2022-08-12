7a7f806dc6f2cc94db082032caca762790c63bff
Chris Hahn <chris+digital@chrishahn.me>
2022-08-11 21:47:51 -0400

    chore: remove schema

--- packages/cms/src/schemas/documents/pages/supportUs.ts
+++
@@ -1,9 +0,0 @@
-import { PageDocument } from '../../../types';
-
-export const schema: PageDocument = {
-    name: 'supportUsPage',
-    title: 'Support Us',
-    icon: '',
-    disabledActions: ['create', 'delete'],
-    fields: [],
-};
